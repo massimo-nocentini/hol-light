6315ff0bf7d1cbecf9f1ae79e76b1ca34e352475
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-01-08 10:26:38 +0100

    Add `mkfifo` interaction to allow an asynchronuous interaction with a background HOL Light process.

---
+++ mkfifo-interaction/start-interaction.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SESSION_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+
+SESSION_FIFO=/tmp/hol-light-session-$SESSION_UUID
+OUTPUT_REDIRECTION=hol-light-redirected-output-session-$SESSION_UUID.out
+KILL_SCRIPT=send-KILL-signal-to-ocaml-background-session-$SESSION_UUID.sh
+
+# start the fifo queue for the asynchronuous interaction
+mkfifo $SESSION_FIFO
+
+# in the `ocaml` command should be added `+I camlp5`
+nohup tail -s 0.1 -f $SESSION_FIFO | ocaml > $OUTPUT_REDIRECTION & 
+OCAML_PID=$!
+
+# make the script to send interrupts to the background process
+echo "kill -SIGINT ${OCAML_PID}" > $KILL_SCRIPT
+chmod +x $KILL_SCRIPT
+
+# prepare the environment, namely begin the interaction, asynchronuously
+echo "#use \"hol.ml\";;" > $SESSION_FIFO
+
+# begin watching
+tail pid=$OCAML_PID -f $OUTPUT_REDIRECTION
