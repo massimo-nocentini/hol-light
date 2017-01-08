572f0ea177214f7078ce15a52e0c9b09cc99bb50
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-01-08 10:39:40 +0100

    Fix the fifo interaction start script and moved it to the root in the repo.

--- mkfifo-interaction.sh
+++ mkfifo-interaction.sh
@@ -3,8 +3,10 @@
 SESSION_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 SESSION_FIFO=/tmp/hol-light-session-$SESSION_UUID
-OUTPUT_REDIRECTION=hol-light-redirected-output-session-$SESSION_UUID.out
-KILL_SCRIPT=send-KILL-signal-to-ocaml-background-session-$SESSION_UUID.sh
+
+BASE_DIR=./mkfifo-sessions
+OUTPUT_REDIRECTION=$BASE_DIR/hol-light-redirected-output-session-$SESSION_UUID.out
+KILL_SCRIPT=$BASE_DIR/send-KILL-signal-to-ocaml-background-session-$SESSION_UUID.sh
 
 # start the fifo queue for the asynchronuous interaction
 mkfifo $SESSION_FIFO
