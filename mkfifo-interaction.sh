a0b92e4532a1a59e48ffb64245703f377cb3c63a
Massimo Nocentini <massimo.nocentini@gmail.com>
2017-05-25 11:02:57 +0200

    Fix script to start interaction using a FIFO queue.

--- mkfifo-interaction.sh
+++ mkfifo-interaction.sh
@@ -8,11 +8,12 @@ BASE_DIR=mkfifo-sessions
 OUTPUT_REDIRECTION=$BASE_DIR/hol-light-redirected-output-session-$SESSION_UUID.out
 KILL_SCRIPT=$BASE_DIR/send-KILL-signal-to-ocaml-background-session-$SESSION_UUID.sh
 
+
 # start the fifo queue for the asynchronuous interaction
 mkfifo $SESSION_FIFO
 
 touch $OUTPUT_REDIRECTION # to ensure that the output file exists
-nohup tail -f $SESSION_FIFO | ocaml -I `camlp5 -where` >> $OUTPUT_REDIRECTION & 
+tail -f $SESSION_FIFO | ocaml -I `camlp5 -where` >> $OUTPUT_REDIRECTION &
 OCAML_PID=$!
 
 # make the script to send interrupts to the background process
@@ -22,5 +23,9 @@ chmod +x $KILL_SCRIPT
 # prepare the environment, namely begin the interaction, asynchronuously
 echo "#use \"hol.ml\";;" > $SESSION_FIFO
 
-# begin watching
-tail -f $OUTPUT_REDIRECTION
+# show the current FIFO pathname
+echo "FIFO queue UUID: $SESSION_UUID"
+echo "to interact with HOL send messages to the FIFO queue '$SESSION_FIFO'"
+echo "to start watching eval 'tail -f $OUTPUT_REDIRECTION'"
+echo "to send interrupts eval 'bash $KILL_SCRIPT'"
+echo "to shutdown the FIFO queue eval 'ps' and kill the process relative to the line that starts with 'tail -f ...'"
